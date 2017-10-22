5afbd94cdf3ba3685031e126ccd46e20b2668345
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-10-22 08:59:47 -0400

    Added comments

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
+//turn on debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
+//activate autoloader function
 function  my_autoloader($class) {
 	include $class . '.php';
 				}
 	
 spl_autoload_register ('my_autoloader');
 
+//Instantiate the program  object
 $obj= new main ();
 
-
+//start main class 
 class main {
+	
 	public function __construct() {
+	
+	//Set default for page request and load the page
 	$pageRequest = 'upload';
 	if(isset($_REQUEST['page'])) {
-	$pageRequest = $_REQUEST['page'];
+		$pageRequest = $_REQUEST['page'];
 		}
+	
 	$page = new $pageRequest;
 
 	if($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -28,15 +35,21 @@ class main {
 }
 
 abstract class page {
+
+	//create html and add edits to the page
 	public $html;
+	
 	public function __construct() {
+		
 		$this->html .='<html>';
-		$this->html .='<linkrel="stylesheet"href="style.css">';
+		$this->html .='<link rel="stylesheet" type="text/css"
+		href="style.css"/>';
 		$this->html .='<body>';
 				
-				}
+		//get and print html table		}
 	public function __destruct() {
-	$this->html .= '</body></html>';
+	
+		$this->html .= '</body></html>';
 	stringFunctions:: printThis($this->html);
 	}
 
@@ -51,15 +64,17 @@ abstract class page {
 
 }
 
+
 class upload extends page {
+
+//create the form allowing to upload the file
 public function get () {
 	$form = '<form enctype="multipart/form-data" action="upload.php" method="post">';
 	$form .= '<input type ="file" name="fileToUpload" id="fileToUpload">';
 	$form .= '<input type ="submit" value="Upload File" name="submit">';
 	$form .= '</form>';
-		$this->html .= '<h2>Form</h2>';
+		$this->html .= '<h2>Upload the File Below</h2>';
 		$this->html .= $form;
-}
+	}
 }
 ?>
-
